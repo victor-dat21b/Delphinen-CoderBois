c1a8fb27517439cca7c6c01cf223d50d02cb2630
victor-dab21b <vict702k@stud.kea.dk>
2021-11-18 11:35:19 +0100

    Sixth commit

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,27 +1,48 @@
 import java.util.Scanner;
 public class UserInterface {
+    Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
-        new UserInterface().startGame();
+        new UserInterface().chooseUser();
     }
 
     public void startGame() {
+
         Scanner in = new Scanner(System.in);
         startScreen();
+        int myInt = chooseUser();
         boolean gameFlag = true;
         while (gameFlag) {
-            System.out.println("What do you wish to do?");
+            if (myInt == 1){
+                foremanUser();
+            } else if (myInt == 2){
+                cashierUser();
+            } else{
+                trainerUser();
+            }
             if (in.hasNextInt()){
                 int userInput = in.nextInt();
                 if (userInput == 1) {
+                    System.out.println("Opret nyt medlem");
 
 
-                } else if (userInput == 0) {
+                } else if (userInput == 2){
+
+                }else if (userInput == 3){
+
+                }else if (userInput == 4){
+
+                }else if (userInput == 5){
+
+                }
+
+                else if (userInput == 0) {
                     gameFlag = false;
                     System.out.println("You exited the game.");
                 }
             } else{
                 System.out.println("Error, try again.");
+                in.nextInt();
             }
 
         }
@@ -36,6 +57,39 @@ public class UserInterface {
                 buildingString.append(System.getProperty("line.separator"));
                 buildingString.append("-----------------------------------------------");
                 System.out.println(buildingString);
+            }
+
+            public int chooseUser(){
+
+            Menu startMenu = new Menu("Choose user:",
+                    "Use numbers.", new String[]{"1 for foreman.", "2 for cashier.", "3 for trainer."});
+            startMenu.printMenu();
+            int myInt = 0;
+                int userInput;
+                if(in.hasNextInt()){
+                    userInput = in.nextInt();
+                    if (userInput == 1){
+                        myInt = 1;
+                    } else if(userInput == 2){
+                        myInt = 2;
+                    }else if(userInput == 3){
+                        myInt = 3;
+                    }
+                } else {
+                    System.out.println("Try again");
+                }
+                return myInt;
+
+            }
+
+            public void foremanUser(){
+
+            }
+            public void cashierUser(){
+
+            }
+            public void trainerUser(){
+
             }
 
 
