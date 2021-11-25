02b805fa20b8db1332a0a5d098429841afdc1822
Lasse-R <lass531n@stud.kea.dk>
2021-11-25 11:04:03 +0100

    attributter + getters og setters på member

--- src/Member.java
+++ src/Member.java
@@ -1,2 +1,39 @@
 public abstract class Member {
+
+    private String name;
+    private int age;
+    private boolean active;
+    private boolean arrears;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public void setArrears(boolean arrears) {
+        this.arrears = arrears;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean getActive() {
+        return active;
+    }
+
+    public boolean getArrears () {
+        return arrears;
+    }
 }
