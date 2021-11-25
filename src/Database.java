70bf00711ff1a5864829bff5da37d2b3acafcb6a
Troels <troe1912@stud.kea.dk>
2021-11-25 10:53:01 +0100

    oprettet Databse med indhold

--- src/Database.java
+++ src/Database.java
@@ -1,3 +1,43 @@
+import java.util.List;
+
 public class Database {
+
+    void getMemberList(){
+
+    }
+
+    void setSwimmerTime(){
+
+    }
+
+    List readFile(){
+
+        return null;
+    }
+
+    void writeFile(){
+
+    }
+
+    void getTopFive(){
+
+    }
+
+    void saveMember(Member member){
+
+    }
+
+    void createMember(){
+
+    }
+
+    void calculateIncome(){
+
+    }
+
+    void calculateArrears(){
+
+    }
+
 
 }
