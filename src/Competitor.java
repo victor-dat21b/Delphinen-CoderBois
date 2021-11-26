6ae7537c583ff17c871dc3d1b80afe251135487e
victor-dab21b <vict702k@stud.kea.dk>
2021-11-26 12:35:55 +0100

    Database

--- src/Competitor.java
+++ src/Competitor.java
@@ -1,13 +1,25 @@
 import java.util.ArrayList;
 
 public class Competitor extends Member implements Comparable<Competitor>{
-    ArrayList<int[]> disciplineAndTimes;
+    private int amountDiscipline;
+    private double crawl;
+    private double rygcrawl;
+    private double brystsvømning;
+    private double butterfly;
 
-    public Competitor(String name, int age, boolean active, boolean arrears, ArrayList<int[]> disciplineAndTime) {
+    public Competitor(String name, int age, boolean active, boolean arrears, int discipline) {
         super(name, age, active, arrears);
-        this.disciplineAndTimes = disciplineAndTime;
+        this.amountDiscipline = discipline;
     }
 
+
+    public void setDisciplineTime(String discipline, double time){
+        //switch der genkender disciplin type ud fra string og sætter tiden ud fra time
+    }
+
+    public int getAmountDiscipline(){
+        return this.amountDiscipline;
+    }
 
     @Override
     public int compareTo(Competitor otherCompetitor) {
--- src/Database.java
+++ src/Database.java
@@ -13,15 +13,41 @@ public class Database {
 
     }
 
-    public ArrayList<Member> readFile(){
+    public ArrayList<Member> readExerciserFile(){
         ArrayList<Member> myMembers = new ArrayList<>();
         try {
-            File myFile = new File("members.csv");
+            File myFile = new File("Exerciser.csv");
             Scanner myReader = new Scanner(myFile);
             while (myReader.hasNextLine()){
                 String[] data = myReader.nextLine().split(",");
                 Member myMember = new Exerciser(data[0], Integer.parseInt(data[1]), Boolean.parseBoolean(data[2]), Boolean.parseBoolean(data[3]));
                 myMembers.add(myMember);
+            }
+        }
+        catch (Exception e){
+            System.out.println("Error");
+            myMembers = null;
+        }
+        return myMembers;
+    }
+
+    public ArrayList<Member> readCompetitorFile(){
+        ArrayList<Member> myMembers = new ArrayList<>();
+        try {
+            File myFile = new File("Competitor.csv");
+            Scanner myReader = new Scanner(myFile);
+            while (myReader.hasNextLine()){
+                String[] data = myReader.nextLine().split(",");
+                Competitor myCompetitor = new Competitor(data[0], Integer.parseInt(data[1]), Boolean.parseBoolean(data[2]), Boolean.parseBoolean(data[3]), Integer.parseInt(data[4]));
+                if (myCompetitor.getAmountDiscipline() == 1){
+
+                }else if(myCompetitor.getAmountDiscipline() == 2){
+
+                }else if(myCompetitor.getAmountDiscipline() == 3){
+
+                }else if(myCompetitor.getAmountDiscipline() == 4){
+
+                }
             }
         }
         catch (Exception e){
