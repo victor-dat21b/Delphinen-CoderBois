37d3b125173891fa08827f6a30d599ae6cea0bed
Troels <troe1912@stud.kea.dk>
2021-11-30 12:05:48 +0100

    opdated option1 metode

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,8 +1,11 @@
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
 public class UserInterface {
     Scanner in = new Scanner(System.in);
     DolphinController controller = new DolphinController();
+
     public static void main(String[] args) {
         new UserInterface().startProgram();
 
@@ -29,32 +32,30 @@ public class UserInterface {
                     option1();
 
                     //Sendes over i en anden klasse med metode som opretter medlem
-                } else if (userInput == 2){
+                } else if (userInput == 2) {
 
                     System.out.println("The expected income is = " + controller.calculateIncome());
 
-                }else if (userInput == 3){
+                } else if (userInput == 3) {
 
                     System.out.println(new FileHandler().readExerciserFile());
                     System.out.println(new FileHandler().readCompetitorFile());
 
-                }else if (userInput == 4){
+                } else if (userInput == 4) {
 
                     System.out.println("Konkurrencetider");
 
                 } else if (userInput == 5) {
 
                     System.out.println("Træningstider");
-                }else if (userInput == 6){
+                } else if (userInput == 6) {
 
                     System.out.println(controller.getTopFive());
-                }else if (userInput == 7){
+                } else if (userInput == 7) {
 
                     System.out.println("Medlemmer i restance:");
                     System.out.println(controller.getArreas());
-                }
-
-                else if (userInput == 0) {
+                } else if (userInput == 0) {
 
                     gameFlag = false;
 
@@ -80,12 +81,13 @@ public class UserInterface {
                 --------------------------------------------------""");
     }
 
-    public void option1(){//Troels
-
+    public void option1() {//Troels
+        String[] diciplins = {"crawl", "rygcrawl", "brystsvømning", "butterfly"};
         StringBuilder stringBuilder = new StringBuilder();
         StringBuilder stringBuilderDeci = new StringBuilder();
         String finalString = "";
         int numberOfDiciplins = 0;
+        boolean isCompetitor = false;
 
         System.out.println("Opret nyt medlem");
         in.nextLine();
@@ -94,15 +96,30 @@ public class UserInterface {
         stringBuilder.append(in.nextLine()).append(',');
 
 
-
         System.out.println("Alder:");
         stringBuilder.append(in.nextLine());
         stringBuilder.append(',');
+        boolean isActive = false;
+        boolean isValid = false;
+        int answer = 0;
 
-        System.out.println("Er medlemmet aktivt?");
-        boolean isActive;
-        String answer = in.nextLine();
-        isActive = answer.equalsIgnoreCase("ja");
+        do {
+            System.out.println("Er medlemmet aktivt?");
+            System.out.println("""
+                    1. Ja
+                    2. Nej
+                    (Brug tal)""");
+            answer = Integer.parseInt(in.nextLine());
+            if (answer == 1) {
+                isActive = true;
+                isValid = true;
+            } else if (answer == 2) {
+                isValid = true;
+            } else {
+                System.out.println("Ikke gyldigt input");
+            }
+        } while (!isValid);
+
 
         if (!isActive) {
             stringBuilder.append("false");
@@ -118,28 +135,64 @@ public class UserInterface {
         stringBuilder.append("false");
         stringBuilder.append(',');
 
-        System.out.println("Ønsker medlemmet at være konkurrence svømmer?");
-        answer = in.nextLine();
+        do {
+            System.out.println("Ønsker medlemmet at være konkurrence svømmer?");
+            System.out.println("""
+                    1. Ja
+                    2. Nej
+                    (Brug tal)""");
+            isValid = false;
+            answer = Integer.parseInt(in.nextLine());
 
-        if (answer.equalsIgnoreCase("ja")) {
-            ArrayList<String> myList = new ArrayList<>();
-            myList.add("crawl"); myList.add("butterfly"); myList.add("brystsvømning");myList.add("rygcrawl");
-            int myCounter = 0;
-            while(myCounter != 4){
-            System.out.println("Ønsker medlemmet at svømme " + myList.get(myCounter));
-            String diciplinAnswer = in.nextLine();
-            if(diciplinAnswer.equalsIgnoreCase("ja")){
-                stringBuilderDeci.append(myList.get(myCounter));
-                stringBuilderDeci.append(',');
-                stringBuilderDeci.append("0.0");
-                stringBuilderDeci.append(',');
-                stringBuilderDeci.append("00-00-0000");
-                stringBuilderDeci.append(',');
-                numberOfDiciplins = numberOfDiciplins +1;
+
+            if (answer == 1) {
+                isCompetitor = true;
+                isValid = true;
+            } else if (answer == 2) {
+                isValid = true;
             } else {
-
+                System.out.println("Ikke gyldigt input");
             }
-            myCounter++;
+        } while (!isValid);
+
+
+        if (answer == 1) {
+            //ArrayList<String> myList = new ArrayList<>();
+            // myList.add("crawl"); myList.add("butterfly"); myList.add("brystsvømning");myList.add("rygcrawl");
+            isValid = false;
+            int diciplinAnswer = 0;
+            List<String> myList = Arrays.asList("crawl", "butterfly", "brystsvømning", "rygcrawl");
+            int myCounter = 0;
+            while (myCounter != 4) {
+                do {
+                    System.out.println("Ønsker medlemmet at svømme " + myList.get(myCounter));
+                    System.out.println("""
+                            1. Ja
+                            2. Nej
+                            (Brug tal)""");
+                    diciplinAnswer = Integer.parseInt(in.nextLine());
+                    answer = diciplinAnswer;
+                    if (answer == 1) {
+                        isValid = true;
+                    } else if (answer == 2) {
+                        isValid = true;
+                    } else {
+                        System.out.println("Ikke gyldigt input");
+                    }
+                } while (!isValid);
+
+                if (diciplinAnswer == 1) {
+                    stringBuilderDeci.append(myList.get(myCounter));
+                    stringBuilderDeci.append(',');
+                    stringBuilderDeci.append("0.0");
+                    stringBuilderDeci.append(',');
+                    stringBuilderDeci.append("00-00-0000");
+                    stringBuilderDeci.append(',');
+                    numberOfDiciplins = numberOfDiciplins + 1;
+                } else {
+
+                }
+                myCounter++;
             }
 
             stringBuilder.append(String.valueOf(numberOfDiciplins));
@@ -149,11 +202,11 @@ public class UserInterface {
             stringBuilder.append(stringBuilderDeci);
         }
 
-        stringBuilder.replace(stringBuilder.length()-1, stringBuilder.length(), " ");
+        stringBuilder.replace(stringBuilder.length() - 1, stringBuilder.length(), " ");
 
         finalString = stringBuilder.toString();
 
-        if (answer.equalsIgnoreCase("ja")) {
+        if (isCompetitor) {
             System.out.println(controller.createMemberC(finalString));
         } else {
             System.out.println(controller.createMemberE(finalString));
