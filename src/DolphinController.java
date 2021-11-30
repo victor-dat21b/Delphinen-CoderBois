c8270ee7ad3598140a82a1a4807e7d12b3162653
Troels <troe1912@stud.kea.dk>
2021-11-30 10:22:15 +0100

    Fjernet gamel kode

--- src/DolphinController.java
+++ src/DolphinController.java
@@ -7,12 +7,7 @@ public class DolphinController {
     public void getMemberList(){
 
     }
-/*
-    public String createMember(String name, int age, boolean active, boolean arrears, ArrayList<String> disciplineAndTime){
-        return domain.createMember(name, age, active, arrears, disciplineAndTime);
 
-    }
-*/
     public String createMemberC(String data){
         return domain.creatMemberC(data);
     }
--- src/DolphinDomain.java
+++ src/DolphinDomain.java
@@ -11,18 +11,5 @@ public class DolphinDomain {
     public String creatMemberE(String data){
         return files.writeExerciserFileTest(data);
     }
-    /*
-    public String createMember(String name, int age, boolean active, boolean arrears, ArrayList<String> disciplineAndTime) {//Troels
-        if (disciplineAndTime == null) {
-            //Motionist
-            Exerciser exerciser = new Exerciser(name, age, active, arrears);
-            return files.writeExerciserFile(exerciser);
-        } else {
 
-            Competitor competitor = new Competitor(name, age, active, arrears, disciplineAndTime.size());
-            return files.writeCompetitorFile(competitor);
-
-        }
-    }
-*/
 }
